0deda37c2993b030f653fe437b31db6314925e53
Victor Tikhovskoi <tihovskoyviktor@icloud.com>
2025-03-07 17:16:55 +0700

    Настроил ALLOWED_HOSTS через environs, добавил .env.example

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ DEBUG = env.bool('DEBUG', default=False)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
